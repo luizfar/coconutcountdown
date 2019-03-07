eda1abe4dfa79192063ad2ce7da85221cd6bfb6c
Luiz Ribeiro <lribeiro@thoughtworks.com>
2019-03-07 15:16:20 -0300

    Update date to arrival time of next travel.

--- public/js/coconutcountdown.js
+++ public/js/coconutcountdown.js
@@ -1,9 +1,24 @@
 var cc = cc || {};
 
 cc.bootstrap = function () {
+  var months = {
+    JANUARY: 0,
+    FEBRUARY: 1,
+    MARCH: 2,
+    APRIL: 3,
+    MAY: 4,
+    JUNE: 5,
+    JULY: 6,
+    AUGUST: 7,
+    SEPTEMBER: 8,
+    OCTOBER: 9,
+    NOVEMBER: 10,
+    DECEMBER: 11
+  };
+
   var now = new Date();
   var utc = new Date(now.getTime() + now.getTimezoneOffset() * 60000);
-  var arrival = new Date(2018, 11, 31, 2, 0);
+  var arrival = new Date(2019, months.APRIL, 3, 8, 30);
 
   console.log(arrival);
   console.log(utc);
